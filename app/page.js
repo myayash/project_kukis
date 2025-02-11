ef4c2e1fe21d72fc816dc81d2428a6133ce42448
Ayas <my4hya@gmail.com>
2025-02-11 20:24:34 +0700

    next and twcss setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+// entry point for nextjs
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>welcome to kukis nextjs</h1>
+    </div>
+  );
+}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-// entry pointt
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-// create root container for rendering react components,
-// served in html file with id 'root'
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
