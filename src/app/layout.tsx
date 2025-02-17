46afcc186fdd12a893da4fe040b34ff853c6dbb0
Ayas <my4hya@gmail.com>
2025-02-17 17:17:07 +0700

    filler basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1>frothe kukis.</h1>
       <p>under maintenance</p>
+      <Link href="/about">about this project</Link>
     </div>
   );
 }
